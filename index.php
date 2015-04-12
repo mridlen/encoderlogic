f22be917351f01b1f41f8d2e11ee9235ee120d7f
Mark Ridlen <mark@ridlen.net>
2015-04-11 23:20:03 -0500

    fixed bug with comment system

--- index.php
+++ index.php
@@ -389,30 +389,25 @@ $(function() {
 				term.echo("Note: do not enclose your comment in quotes unless you are quoting somebody or for purposes of irony");
 				term.echo("");
 			} else if (currentTrack['trackId'] != 0) {
-				term.echo(cmd.substring(8));
-				SC.connect(function(error) {
-						commentTimestamp = parseInt(Date.now() - currentTrack['startedTimestamp']);
-						postUrl = String('/tracks/' + currentTrack['trackId'] + '/comments');
-						bodyString = String(cmd.substring(8));
-						
-						term.echo("commentTimestamp: " + commentTimestamp);
-						term.echo("postUrl: " + postUrl);
-						term.echo("bodyString: " + bodyString);
-						SC.post(postUrl, {
-							comment: {
-								body: bodyString,
-								timestamp: commentTimestamp
-						    }},
-							function(comment, error) {
-								if(error) {
-									term.echo("Error: " + error.message);
-								}
-							}
-						);
-					if (error) {
-						term.echo("Error: " + error.message);
+				term.echo("comment: " + cmd.substring(8));
+				commentTimestamp = parseInt(Date.now() - currentTrack['startedTimestamp']);
+				postUrl = String('/tracks/' + currentTrack['trackId'] + '/comments');
+				bodyString = String(cmd.substring(8));
+				
+				term.echo("commentTimestamp: " + commentTimestamp);
+				term.echo("postUrl: " + postUrl);
+				term.echo("bodyString: " + bodyString);
+				SC.post(postUrl, {
+					comment: {
+						body: bodyString,
+						timestamp: commentTimestamp
+					}},
+					function(comment, error) {
+						if(error) {
+							term.echo("Error: " + error.message);
+						}
 					}
-				});
+				);
 			}
 		}
 		if (cmd.split(" ")[0] == 'api') {
