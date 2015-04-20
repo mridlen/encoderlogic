7b94f51aaebd66a3092352dbffaf6951f3689038
Mark Ridlen <mark@ridlen.net>
2015-04-19 23:13:12 -0500

    fixed bug with track search, it needed to also include genre property

--- index.php
+++ index.php
@@ -460,7 +460,7 @@ $(function() {
                 term.echo("(no arguments): displays 20 most recent uploaded tracks");
                 term.echo("help: show this menu");
                 term.echo("view: go to the soundcloud page of the " + arg0);
-                term.echo("search (search string): searches the tracks with the search string in the title or in the tags");
+                term.echo("search (search string): searches the tracks with the search string in the title or tags or genre");
                 term.echo("");
 			} else if (arg1 == 'view') {
 				if (arg0 == 'tracks') {
@@ -477,8 +477,10 @@ $(function() {
                     //clear searchTracks[]
                     searchTracks = [];
                     for (i = 0; i < tracks.length; i++) {
-                        if (tracks[i].title.toLowerCase().search(searchString.toLowerCase()) >= 0 || tracks[i].tag_list.toLowerCase().search(searchString.toLowerCase()) >= 0) {
-                            term.echo((i+1) + ") " + tracks[i].id + " - " + tracks[i].user.username  + " - " + tracks[i].title);
+						//console.log("track: " + tracks[i].title + ", string: " + tracks[i].tag_list.toLowerCase() + ", >> result: " + tracks[i].tag_list.toLowerCase().search(searchString.toLowerCase()));
+						
+                        if (tracks[i].title.toLowerCase().search(searchString.toLowerCase()) != -1 || tracks[i].tag_list.toLowerCase().search(searchString.toLowerCase()) != -1 || tracks[i].genre.toLowerCase().search(searchString.toLowerCase()) != -1) {
+                            term.echo("[[;"+ theme['quickIdColor'] +";]" + (i+1) + ")] [[;"+ theme['trackIdColor'] +";]" +  tracks[i].id + "] - [[;"+ theme['artistIdColor'] +";]" + tracks[i].user.username  + "] - " + tracks[i].title);
 							if (showLinks == 1) {
 								term.echo('\tlink:' + tracks[i].permalink_url);
 							}
