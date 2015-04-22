03b0b69984a8db0ca7396b2e46749c526835df0d
Mark Ridlen <mark@ridlen.net>
2015-04-22 15:02:01 -0500

    added notes to main.js

--- modules/main.js
+++ modules/main.js
@@ -1,4 +1,7 @@
 //This file loads all the modules for the terminal commands
 
+//global variables
 require(['globals']);
+
+//the terminal application (hopefully I will find a way to break this down into smaller files eventually)
 require(['terminal']);
