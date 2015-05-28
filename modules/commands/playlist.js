8550ce34d2a43e06f471e2cbcc81f7cd885b12b7
Mark Ridlen <mark@ridlen.net>
2015-05-28 19:33:16 +0000

    fixed the playlist command

--- modules/commands/playlist.js
+++ modules/commands/playlist.js
@@ -3,7 +3,7 @@ commands.push({
         {
             trigger: "playlist",
             alias: ["playl", "plali", "plalis"],
-            help: "playlist Name .......... create a playlist with the current playing track and queue.",
+            help: "playlist Name .......... create a playlist with the current playing track and/or queue.",
             requireLoggedIn: true
         }
     ],
@@ -13,7 +13,14 @@ commands.push({
             term.echo("Please specify a playlist name.\n");
             term.echo("Syntax: playlist Name of Playlist");
         } else {
-            var tempQueue = queue.map(function(id) {
+            var tempQueue = queue;
+            
+            //splice in the currently playing track at top
+            tempQueue.unshift(currentTrack['trackId']);
+            
+            //I have very little idea what this map function does, but it was in the example code from soundcloud
+            //https://developers.soundcloud.com/docs/api/guide#creating-sets
+            var tempQueue = tempQueue.map(function(id) {
                 return { id: id }
             });
             
